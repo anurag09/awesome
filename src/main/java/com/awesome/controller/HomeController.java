c4a28545efb37c79d687eeabc7a9df87d8a82d16
amm0924 <anurag.mishra@springernature.com>
2017-03-07 14:09:32 +0530

    Implemented login

--- src/main/java/com/awesome/controller/HomeController.java
+++ src/main/java/com/awesome/controller/HomeController.java
@@ -4,17 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author ekansh
- * @since 30/3/16
- */
 @Controller
 @RequestMapping({"","/"})
 public class HomeController {
 
     @RequestMapping("")
     public String home(){
-
         return "welcome";
     }
 
--- src/main/java/com/awesome/controller/UserController.java
+++ src/main/java/com/awesome/controller/UserController.java
@@ -4,10 +4,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @author ekansh
- * @since 30/3/16
- */
 @RestController
 @RequestMapping("/user")
 @PreAuthorize("hasAuthority('USER')")
