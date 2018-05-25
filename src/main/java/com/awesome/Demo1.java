49d57dd7bf1b0c9413c825d1e73d6bb944b11a3c
anurag09 <mishra.anurag09@gmail.com>
2018-05-25 15:01:19 +0530

    DEmo1 created

---
+++ src/main/java/com/awesome/Demo1.java
@@ -0,0 +1,13 @@
+package com.awesome;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Anurag Mishra on 25-05-2018.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+    }
+}
